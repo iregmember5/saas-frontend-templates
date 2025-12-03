99282b3903e42e2481cfa26533e369662932f50f
dev1IREG61 <ireg.member61@gmail.com>
2025-12-03 14:26:19 +0500

    Changes

--- templates-frontend/w9-chaser-template/src/App.tsx
+++ templates-frontend/w9-chaser-template/src/App.tsx
@@ -74,7 +74,6 @@ function App() {
 
     checkRoute();
 
-    // Listen for hash changes (for navigation without page reload)
     window.addEventListener("hashchange", checkRoute);
     window.addEventListener("popstate", checkRoute);
 
